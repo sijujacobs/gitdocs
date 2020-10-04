1fc1d15139214ed5a8f0b359a48a770dbecc2382
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-10-04 08:20:56 -0400

    user1 commit oct4 0820AM

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   };
   const user2Function = () => {
     console.log("user2Function-----2----------");
-    console.log("user1Function-----1.11123----------");
+    console.log("user1Function-----oct4 0731AM----------");
   };
 
   user1Function();
