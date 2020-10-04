55ea6a84ce122b6418f4ad7609296c3018f46b3a
roselinjacobs <roselinjacobs@gmail.com>
2020-10-04 07:53:44 -0400

    user2 commit oct4 0753AM

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   };
   const user2Function = () => {
     console.log("user2Function-----2----------");
-    console.log("user2Function---22222222--oct4 0735AM----------");
+    console.log("user2Function---22222222--oct4 0735AM---2-------");
   };
 
   user1Function();
